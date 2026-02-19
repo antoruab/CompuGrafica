c3812bbb8ae6aaca90ada57763cc6e2ab1009387
antoruab <antoniarua2007@gmail.com>
2026-02-19 11:48:46 -0500

    Ejemplo cubo
    Ejemplo cubo, orbita, rotación

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,50 @@
-alert("Main.js");
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xFFFFFF);
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+//Primera figura geométrica
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+//Control orbita
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.z = 5;
+controls.update();
+
+//Rotación del cubo
+function animate(time) {
+    cube.rotation.x = time / 2000;
+    cube.rotation.y = time / 1000;
+    controls.update();
+    renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
+
+//////////////////////////////
+const size = 10;
+const divisions = 10;
+const gridHelper = new THREE.GridHelper(size, divisions);
+scene.add(gridHelper);
+/////////////////////77777777777
+
+//Ajustar tamaño ventana
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
