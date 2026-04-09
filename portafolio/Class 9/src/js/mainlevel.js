bb436b3974a503311b008477bf57319a9bda1630
antoruab <antoniarua2007@gmail.com>
2026-04-09 11:44:58 -0500

    game2

--- portafolio/Class 9/src/js/mainlevel.js
+++ portafolio/Class 9/src/js/mainlevel.js
@@ -1,11 +1,78 @@
+import * as THREE from 'three';
+import Stats from 'three/addons/libs/stats.module.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const timer = new THREE.Timer();
+timer.connect(document);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x7BD0F7);
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+// Light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(5, 10, 7.5);
+scene.add(directionalLight);
+
+
+camera.position.z = 8;
+camera.position.y = -1;
+
+const stats = new Stats();
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+
+function animate(time) {
+    timer.update();
+    stats.update();
+    renderer.render(scene, camera);
+}
+
+// Load Scene //
+const loader = new GLTFLoader();
+// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( '../src/models/glb' );
+loader.setDRACOLoader( dracoLoader );
+const gltf = await loader.loadAsync( '../src/models/glb/Casita.glb' );
+
+gltf.scene.position.set(0, -2.5, 3.5);
+gltf.scene.rotation.y = Math.PI*1.5; // Rota 180 grados en el eje Y
+scene.add( gltf.scene );
+
+//////////////////////////////////////////////
+window.addEventListener('resize', onWindowResize, false);
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+////////////////////////////////////////
+
 // AUDIO play
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     var audio = document.getElementById('miAudio2');
-    var reproducir = function() {
-        audio.play().then(function() {
+    var reproducir = function () {
+        audio.play().then(function () {
             document.removeEventListener('click', reproducir);
             document.removeEventListener('keydown', reproducir);
-        }).catch(function(error) {
+        }).catch(function (error) {
             console.log("Esperando interacción real...");
         });
     };
